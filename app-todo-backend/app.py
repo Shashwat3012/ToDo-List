cb2af4176530c0f444e56c0db6d7b0fe3547f18c
Shashwat Tripathi <2021.shashwat.tripathi@ves.ac.in>
2023-05-21 19:30:50 +0530

    Updated update API

--- app-todo-backend/app.py
+++ app-todo-backend/app.py
@@ -1,17 +1,14 @@
+
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-from pymongo import MongoClient
 import sys
 import pymongo
+import datetime
 
 app = Flask(__name__)
 CORS(app)
 
 # MongoDB connection
-# client = MongoClient("mongodb+srv://shashwattripathi:0VxcsURVQyK6ULhD@localghost.i2ouhm1.mongodb.net/?retryWrites=true&w=majority")
-# db = client['task_list']
-# todos_collection = db['todo-app']
-
 try:
     client = pymongo.MongoClient(
         "mongodb+srv://shashwattripathi:0VxcsURVQyK6ULhD@localghost.i2ouhm1.mongodb.net/?retryWrites=true&w=majority")
@@ -21,20 +18,12 @@ except pymongo.errors.ConfigurationError:
     print("An Invalid URI host error was received. Is your Atlas host name correct in your connection string?")
     sys.exit(1)
 
-# use a database named "myDatabase"
+# use a database named "taskList"
 db = client.taskList
 
-# use a collection named "recipes"
+# use a collection named "todo-app"
 todos_collection = db["todo-app"]
-
-recipe_documents = [
-    {"name": "elotes", "ingredients": ["corn", "mayonnaise", "cotija cheese", "sour cream", "lime"], "prep_time": 35},
-    {"name": "loco moco", "ingredients": ["ground beef", "butter", "onion", "egg", "bread bun", "mushrooms"],
-     "prep_time": 54},
-    {"name": "patatas bravas", "ingredients": ["potato", "tomato", "olive oil", "onion", "garlic", "paprika"],
-     "prep_time": 80},
-    {"name": "fried rice", "ingredients": ["rice", "soy sauce", "egg", "onion", "pea", "carrot", "sesame oil"],
-     "prep_time": 40}]
+count = 1
 
 
 @app.route('/api/todo-app', methods=['GET'])
@@ -52,22 +41,20 @@ def get_todo():
 
 @app.route('/api/todo-app', methods=['POST'])
 def add_todo():
-    # new_todo = request.json['text']
-    # todos_collection.insert_one({'text': 'new_todo'})
-    # return '', 201
-    print("helloooo I am here!!!!!!")
+    global count
     try:
         new_todo = request.json['text']
-        print(new_todo)
-        #task = {"task": new_todo, "added_time": , "completed_time": }
-        todos_collection.insert_one({'text': new_todo})
-        # result = todos_collection.insert_many(recipe_documents)
+        id = count
+        data = {'id': id, 'task': (new_todo,), 'added_time': str(datetime.datetime.now())}
+        # json_data = json.dumps(data)
+        todos_collection.insert_one(data)
     # returning a friendly error if the operation fails
     except pymongo.errors.OperationFailure:
         print(
             "An error was received while adding a new record.")
         sys.exit(1)
     else:
+        count += 1
         print("I inserted a new record")
         print("\n")
     return 'Successfully added new task', 201
@@ -75,21 +62,20 @@ def add_todo():
 
 @app.route('/api/todo-app/<id>', methods=['PUT'])
 def edit_todo(id):
+    filter = {'id': id}
+    new_todo = request.json['newTask']
+    # Values to be updated.
+    newvalues = {"$set": {'task': new_todo}}
 
-    return '', 201
+    # Using update_one() method for single updation.
+    todos_collection.update_one(filter, newvalues)
+    return 'Updated task successfully', 200
 
 
 @app.route('/api/todo-app/<id>', methods=['DELETE'])
 def delete_todo(id):
     todos_collection.delete_one({'_id': id})
     return '', 204
-
-
-def sort_todo(id):
-    def get_sorted_tasks(sort_field):
-        tasks = todos_collection.find().sort(sort_field)
-        return list(tasks)
-
 
 if __name__ == '__main__':
     # app.run()
