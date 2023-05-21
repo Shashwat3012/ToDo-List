188e52527d67cc6d1498fe9638826e62603483ef
Shashwat Tripathi <2021.shashwat.tripathi@ves.ac.in>
2023-05-21 15:41:37 +0530

    Initial UI changes

---
+++ app-todo/src/App.js
@@ -0,0 +1,61 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const fetchTodos = async () => {
+    try {
+      const response = await axios.get('/api/todo-app');
+      setTodos(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const addTodo = async () => {
+    try {
+      await axios.post('/api/todo-app', { "text": "newTodo" });
+      // fetchTodos();
+      // setNewTodo('');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const deleteTodo = async (id) => {
+    try {
+      await axios.delete(`/api/todo-app/${id}`);
+      fetchTodos();
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>To-Do List</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo._id}>
+            {todo.text}
+            <button onClick={() => deleteTodo(todo._id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+      />
+      <button onClick={addTodo}>Add</button>
+    </div>
+  );
+};
+
+export default App;
