bdc7d67e3935b396018c2da670662df83069362b
Shashwat Tripathi <2021.shashwat.tripathi@ves.ac.in>
2023-05-21 18:21:11 +0530

    Updated UI code

--- app-todo/src/App.js
+++ app-todo/src/App.js
@@ -2,8 +2,10 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 const App = () => {
-  const [todos, setTodos] = useState([]);
-  const [newTodo, setNewTodo] = useState('');
+  const [tasks, setTasks] = useState([]);
+  const [newTask, setNewTask] = useState('');
+  const [editedTask, setEditedTask] = useState('');
+  const [editMode, setEditMode] = useState(false);
 
   useEffect(() => {
     fetchTodos();
@@ -11,16 +13,40 @@ const App = () => {
 
   const fetchTodos = async () => {
     try {
-      const response = await axios.get('/api/todo-app');
-      setTodos(response.data);
+      const response = await axios.get('http://localhost:3001/api/todo-app');
+      response.data.forEach(data => {
+        setTasks([...tasks, data.text]);
+       // setTasks(data);
+      });
+      
     } catch (error) {
       console.error(error);
     }
   };
 
-  const addTodo = async () => {
+  const addTask = async () => {
     try {
-      await axios.post('/api/todo-app', { "text": "newTodo" });
+      await axios.post('http://localhost:3001/api/todo-app', { "text": newTask });
+      fetchTodos();
+      setNewTask('');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const editTask = async () => {
+    try {
+      await axios.post('http://localhost:3001/api/todo-app', { "text": "newTodo" });
+      fetchTodos();
+      setNewTask('');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const updateTask = async () => {
+    try {
+      await axios.post('http://localhost:3001/api/todo-app', { "text": "newTodo" });
       // fetchTodos();
       // setNewTodo('');
     } catch (error) {
@@ -28,7 +54,17 @@ const App = () => {
     }
   };
 
-  const deleteTodo = async (id) => {
+  const sortTasks = async () => {
+    // try {
+    //   await axios.post('http://localhost:3001/api/todo-app', { "text": "newTodo" });
+    //   fetchTodos();
+    //   setNewTodo('');
+    // } catch (error) {
+    //   console.error(error);
+    // }
+  };
+
+  const deleteTask = async (id) => {
     try {
       await axios.delete(`/api/todo-app/${id}`);
       fetchTodos();
@@ -37,23 +73,64 @@ const App = () => {
     }
   };
 
-  return (
-    <div>
-      <h1>To-Do List</h1>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo._id}>
-            {todo.text}
-            <button onClick={() => deleteTodo(todo._id)}>Delete</button>
+//   return (
+//     <div>
+//       <h1>To-Do List</h1>
+//       <ul>
+//         {todos.map((todo) => (
+//           <li key={todo._id}>
+//             {todo.text}
+//             <button onClick={() => deleteTodo(todo._id)}>Delete</button>
+//           </li>
+//         ))}
+//       </ul>
+//       <input
+//         type="text"
+//         value={newTodo}
+//         onChange={(e) => setNewTodo(e.target.value)}
+//       />
+//       <button onClick={addTodo}>Add</button>
+//     </div>
+//   );
+
+return (
+    <div style={{ fontFamily: 'Arial, sans-serif', textAlign: 'center' }}>
+      <h1 style={{ fontSize: '28px', margin: '20px 0' }}>Todo List</h1>
+      <div style={{ marginBottom: '10px' }}>
+        <input
+          type="text"
+          value={newTask}
+          onChange={(e) => setNewTask(e.target.value)}
+          placeholder="Enter a new task"
+          style={{ padding: '6px', marginRight: '5px', borderRadius: '4px', border: '1px solid #ccc' }}
+        />
+        <button onClick={addTask} style={{ padding: '6px 12px', borderRadius: '4px', backgroundColor: '#4CAF50', color: '#fff', border: 'none' }}>Add Task</button>
+      </div>
+      <ul style={{ listStyleType: 'none', padding: '0' }}>
+        {tasks.map((task, index) => (
+          <li key={index} style={{ margin: '5px 0', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+            <span style={{ flex: '1' }}>{task}</span>
+            <button onClick={() => editTask(index)} style={{ margin: '0 5px', padding: '4px 8px', backgroundColor: '#2196F3', color: '#fff', border: 'none', cursor: 'pointer' }}>Edit</button>
+            <button onClick={() => deleteTask(index)} style={{ margin: '0 5px', padding: '4px 8px', backgroundColor: '#f44336', color: '#fff', border: 'none', cursor: 'pointer' }}>Delete</button>
           </li>
         ))}
       </ul>
-      <input
-        type="text"
-        value={newTodo}
-        onChange={(e) => setNewTodo(e.target.value)}
-      />
-      <button onClick={addTodo}>Add</button>
+      <div style={{ marginTop: '10px' }}>
+        {editMode ? (
+          <>
+            <input
+              type="text"
+              value={editedTask}
+              onChange={(e) => setEditedTask(e.target.value)}
+              placeholder="Edit task"
+              style={{ padding: '6px', marginRight: '5px', borderRadius: '4px', border: '1px solid #ccc' }}
+            />
+            <button onClick={updateTask} style={{ padding: '6px 12px', borderRadius: '4px', backgroundColor: '#4CAF50', color: '#fff', border: 'none' }}>Update Task</button>
+          </>
+        ) : (
+          <button onClick={sortTasks} style={{ padding: '6px 12px', borderRadius: '4px', backgroundColor: '#FF9800', color: '#fff', border: 'none' }}>Sort Tasks</button>
+        )}
+      </div>
     </div>
   );
 };
